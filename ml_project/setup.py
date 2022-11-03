dd5b4aceb9e01153d52ae185fbe2f947bdd1ca62
AmpiroMax <patrat2015@yandex.ru>
2022-11-03 17:12:28 +0300

    Data preporation and setup.py were added

--- ml_project/setup.py
+++ ml_project/setup.py
@@ -6,5 +6,13 @@ setup(
     version='0.1.0',
     description='Simple ML project for VK MLOps course.',
     author='Ampiro',
+    entry_points={
+        "console_scripts": [
+            "train=src.models.console_functions.pipeline:commands_runner",
+            "predict=src.models.console_functions.pipeline:commands_runner",
+            "evaluate=src.models.console_functions.pipeline:commands_runner",
+            "pipeline=src.models.console_functions.pipeline:pipeline_command"
+        ]
+    },
     license='',
 )
--- ml_project/src/data_preporation/__init__.py
+++ ml_project/src/data_preporation/__init__.py
@@ -1,6 +1,16 @@
-from .make_dataset import make_dataset, get_features_target
+from .data_transformer import CustomTransformer
+from .data_functionality import (
+    load_data,
+    make_dataset,
+    get_features_target
+)
+from .read_config import read_config
+
 
 __all__ = [
+    "load_data",
     "make_dataset",
-    "get_features_target"
+    "get_features_target",
+    "read_config",
+    "CustomTransformer"
 ]
